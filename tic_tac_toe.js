76b49c678662dd181e4b4552b5e1fda5cad21d19
hi-kaye <kayehin@gmail..com>
2021-05-18 17:54:16 +0100

    updated

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,12 +1,12 @@
-const readline = require("readline");
+const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 //players
-const playerOne = "X";
-const playerTwo = "O";
+const playerOne = 'X';
+const playerTwo = 'O';
 
 let player = playerOne;
 
@@ -20,26 +20,25 @@ for (var i = 0; i < board.length; i++) {
 function printBoard() {
   let printableBoard = [];
   for (slice = 0; slice < board.length; ) {
-    const row = board.slice(slice, (slice += 3)).join(" | ") + "\n";
+    const row = board.slice(slice, (slice += 3)).join(' | ') + '\n';
     printableBoard.push(row);
   }
-  console.log(printableBoard.join("----------\n"));
+  console.log(printableBoard.join('----------\n'));
 }
 
 //game loop
 function playGame() {
   let gameOn = false;
   for (i = 0; i < board.length; i++) {
-    if (board[i] !== "X" || board[i] !== "O") {
+    if (board[i] !== 'X' || board[i] !== 'O') {
       gameOn = true;
-    }
-  }
-
+    } 
+  } 
   if (gameOn) {
-    rl.question("Play your move: ", function (pos) {
+    rl.question('Play your move: ', function (pos) {
       playMove(pos);
     });
-  }
+  } 
 }
 
 //play move and change players
@@ -47,17 +46,21 @@ function playMove(pos) {
   let actualPosition = pos - 1;
   //console.log(checkIsInt(actualPosition))
 
-  //checks if postion is taken
+
+  //check if postion is taken
   if (!checkAvailable(actualPosition)) {
-    console.log("Position taken! Please pick available position ");
+    console.log('Position taken! Please pick available position ');
   }
 
   //check if entered position is valid
   if (!checkIsInt(pos)) {
-      console.log("Enter a valid position between 1-9 ")
+      console.log('Enter a valid position between 1-9 ')
   }
 
   let i;
+  console.log(checkDraw())
+
+
   for (i = 0; i < board.length; i++) {
     if (actualPosition == i && checkAvailable(actualPosition) && checkIsInt(actualPosition)) {
       board[i] = player;
@@ -70,25 +73,25 @@ function playMove(pos) {
     playGame();
   }
 
-  console.log("\n");
+  console.log('\n');
   printBoard();
 
   const winner = checkWinner();
-  if (winner !== " ") {
+  if (winner !== ' ') {
     console.log(`${winner} is the winner!`);
     process.exit(0);
-  }
+  } 
   playGame();
 }
 
 //checks if the position available
 function checkAvailable(pos) {
-  return board[pos] !== "X" && board[pos] !== "O";
+  return board[pos] !== 'X' && board[pos] !== 'O';
 }
 
 //checks if position entered is a number or is more than length of array
 function checkIsInt(pos) {
-  if (isNaN(pos) || pos > 8) {
+  if (isNaN(pos) || pos > board.length) {
     return false;
   }
   return true;
@@ -107,13 +110,22 @@ function checkWinner() {
   } else if (board[1] === board[4] && board[1] === board[7]) {
     return board[1];
   } else if (board[2] === board[5] && board[2] === board[8]) {
-    return (winner = board[2]);
+    return board[2];
   } else if (board[0] === board[4] && board[0] === board[8]) {
     return board[0];
   } else if (board[2] === board[4] && board[2] === board[6]) {
     return board[2];
   }
   return ' ';
+}
+
+//check for draw
+function checkDraw() {
+  for (i = 1; i < board.length; i++) {
+      if (typeof board[i] === 'number') {
+        return true
+      } return false
+  } 
 }
 
 printBoard();
